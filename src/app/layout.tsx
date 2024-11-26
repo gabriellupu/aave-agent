8688d08e145a8429da62669d0c48df921dcc8e0a
Alex <alexastrum@gmail.com>
2024-11-26 15:46:27 -0500

    use chadcn/ui dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,12 +11,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className="min-h-screen bg-background">{children}</body>
     </html>
   );
 }
