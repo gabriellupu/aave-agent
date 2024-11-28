0d38b2508d5ddac38f0f2c09e4c2f7cb7add5fde
Alex <alexastrum@gmail.com>
2024-11-28 16:55:46 -0500

    auto-append .near to accountId

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -6,9 +6,13 @@ import { getUserKarma } from "@/lib/user-karma";
 const app = new Elysia({ prefix: "/api", aot: false })
   .use(swagger())
   .get("/karma/:account", async ({ params: { account } }) => {
-    // TODO: if account has no suffix, then append .near
-    const accountId = account;
-    const karma = getUserKarma(accountId);
+    // if account has no suffix, then append .near,
+    // but only if no suffix present and if the account is not a hash
+    let accountId = account;
+    if (!account.includes(".") && !/^[0-9a-fA-F]{64}$/.test(account)) {
+      accountId = `${account}.near`;
+    }
+    const karma = await getUserKarma(accountId);
     if (!karma) {
       return {
         error: `Token ${account} not found`,
